4d7f458949753ae1f47c87b0d229fde8485a1cb3
Andreas Drobisch <dro@unkonstant.de>
2015-08-20 09:29:49 +0200

    fix(scripting): proper setting of context attribute, add debug logging

--- execution/src/main/java/brainslug/flow/execution/node/task/ScriptExecutor.java
+++ execution/src/main/java/brainslug/flow/execution/node/task/ScriptExecutor.java
@@ -3,15 +3,19 @@ package brainslug.flow.execution.node.task;
 import brainslug.flow.context.ExecutionContext;
 import brainslug.flow.node.task.TaskScript;
 import brainslug.util.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.script.Bindings;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.StringWriter;
 
 public class ScriptExecutor {
 
     ScriptEngineManager manager;
+    Logger log = LoggerFactory.getLogger(ScriptExecutor.class);
 
     public ScriptExecutor(ScriptEngineManager manager) {
         this.manager = manager;
@@ -29,12 +33,30 @@ public class ScriptExecutor {
 
     protected Object evalScript(TaskScript taskScript, ScriptEngine scriptEngine, ExecutionContext context) {
         try {
-            Bindings bindings = scriptEngine.createBindings();
-            bindings.put("context", context);
-            return scriptEngine.eval(taskScript.getText(), bindings);
+            StringWriter outputWriter = new StringWriter();
+            StringWriter errorWriter = new StringWriter();
+
+
+            ScriptContext scriptContext = scriptEngine.getContext();
+            scriptContext.setWriter(outputWriter);
+            scriptContext.setErrorWriter(errorWriter);
+
+            scriptContext.setAttribute("context", context, ScriptContext.ENGINE_SCOPE);
+
+            Object result = scriptEngine.eval(taskScript.getText(), scriptContext);
+
+            log.debug("script-out", "script output: " + writerContent(outputWriter));
+            log.debug("script-err", "error output: " + writerContent(errorWriter));
+
+            return result;
         } catch (ScriptException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private String writerContent(StringWriter outputWriter) {
+        outputWriter.flush();
+        return outputWriter.getBuffer().toString();
     }
 
     public Option<ScriptEngine> getScriptEngine(String language) {
